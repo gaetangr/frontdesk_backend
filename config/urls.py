f6cf378385c7951c6170bae7c35ed35837a5de35
Gaëtan <39266116+Mcflan-7@users.noreply.github.com>
2021-01-25 11:16:35 +0100

    Add endpoint documentation to project

--- config/urls.py
+++ config/urls.py
@@ -6,12 +6,22 @@ from django.urls import include, path
 from django.utils.html import format_html
 from django.utils.translation import gettext as _
 from django.views.generic import TemplateView
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions
-from rest_framework.schemas import get_schema_view
+from drf_yasg import openapi  # new
+from drf_yasg.views import get_schema_view  # new
+from rest_framework import permissions  # new
 
 from frontdesk.property.models import Property
+
+schema_view = get_schema_view(  # new
+    openapi.Info(
+        title="Front desk API",
+        default_version="v1",
+        description="Endpoint for the front desk API",
+        contact=openapi.Contact(name="Gaëtan", email="hello@gaetangr.me"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     # Admin routes
@@ -28,9 +38,21 @@ urlpatterns = [
     path("api/v1/auth/registration/", include("dj_rest_auth.registration.urls")),
     # Misc
     # ---------------------------------------
+    path(
+        "endpoint/",
+        schema_view.with_ui("swagger", cache_timeout=0),  # new
+        name="schema-swagger-ui",
+    ),
+    path(
+        "redoc/",
+        schema_view.with_ui("redoc", cache_timeout=0),  # new
+        name="schema-redoc",
+    ),
 ]
 
 # Custom header and information to keep us updated on the plateform
 admin.site.site_title = _("Front Desk - Web plateform")
 admin.site.site_header = "Front Desk - Web Plateform"
-admin.site.index_title = format_html("Administration Front Desk | <a href='https://trello.com/b/C4oeeKc3/front-desk-road-map'> Trello </a> - <a href='https://github.com/gaetangr/frontdesk_V2'> Github </a> ")
+admin.site.index_title = format_html(
+    "Administration Front Desk | <a href='https://trello.com/b/C4oeeKc3/front-desk-road-map'> Trello </a> - <a href='https://github.com/gaetangr/frontdesk_V2'> Github </a> "
+)
