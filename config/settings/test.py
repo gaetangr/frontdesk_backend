fcbb9cf57b4d982d146c42c0359809c373f6bce3
Gaëtan <39266116+Mcflan-7@users.noreply.github.com>
2021-02-08 13:04:01 +0100

    remove travis

--- config/settings/test.py
+++ config/settings/test.py
@@ -1,15 +1,19 @@
 """ Settings so github workflow run with no trouble """
-
+import os
 
 from .base import *
 
+
+ALLOWED_HOSTS = ["*"]
+SECRET_KEY = "something"
+DEBUG = False
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '',
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
-    },
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv("DATABASE_NAME", default="name"),
+        "USER": os.getenv("DATABASE_USER", default="user"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD", default="password"),
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
 }
