f867551c39a6ba678e57ffd3d62c9b4fd5aa9a1a
Gaëtan <39266116+Mcflan-7@users.noreply.github.com>
2021-02-15 10:51:41 +0100

    test ci

--- config/settings/test.py
+++ config/settings/test.py
@@ -1,18 +1,17 @@
 """ Settings so github workflow run with no trouble """
-from .base import *
 import os
 
+
 ALLOWED_HOSTS = ["*"]
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = "something"
 DEBUG = False
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv("DATABASE_NAME", default="TEST"),
-        "USER": os.getenv("DATABASE_USER", default="GAETAN"),
-        "PASSWORD": os.getenv("DATABASE_PASSWORD", default="something"),
+        "NAME": os.getenv("DATABASE_NAME", default=""),
+        "USER": os.getenv("DATABASE_USER", default=""),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD", default=""),
         "HOST": "localhost",
         "PORT": "5432",
     }
 }
-
