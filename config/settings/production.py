0d1d19fcb10082d92e8376ad0abf5cd5fdd73dab
Gaëtan <39266116+Mcflan-7@users.noreply.github.com>
2021-02-15 16:09:53 +0100

    Fix production settings and invalid tag in html admin page

--- config/settings/production.py
+++ config/settings/production.py
@@ -14,10 +14,29 @@ from sentry_sdk.integrations.logging import LoggingIntegration
 from .base import *
 
 
-ALLOWED_HOSTS = ["134.209.35.134", "front-desk.fr", "www.front-desk.fr"]
 
-DEBUG = False
+# GENERAL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+SECRET_KEY = env("DJANGO_SECRET_KEY")
+# https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = env.list(
+    "DJANGO_ALLOWED_HOSTS", default=["front-desk.app"]
+)
 
+# ADMIN
+# ------------------------------------------------------------------------------
+# Django Admin URL regex.
+ADMIN_URL = env("DJANGO_ADMIN_URL")
+
+
+# DATABASES
+# ------------------------------------------------------------------------------
+DATABASES["default"] = env.db("DATABASE_URL")  # noqa: F405
+DATABASES["default"]["ATOMIC_REQUESTS"] = True  # noqa: F405
+DATABASES["default"]["CONN_MAX_AGE"] = env.int(  # noqa: F405
+    "CONN_MAX_AGE", default=60
+)
 # EMAIL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
